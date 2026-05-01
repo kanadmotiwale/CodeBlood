a74c78e674195b01c0fc28cd73ce25019e3c0870
Kanad Motiwale <kanad@Kanads-MacBook-Air.local>
2026-04-30 21:24:43 -0400

    test addition for deployment check 2

--- backend/main.py
+++ backend/main.py
@@ -10,8 +10,8 @@ app = FastAPI(title="CodeBlood API", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://code-blood.vercel.app/"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
